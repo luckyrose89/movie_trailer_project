0ae6d5527b9905f08eb2ca1696ec2bf1d2c99f56
Divya Mathur <avidrutham@gmail.com>
2017-09-17 13:58:25 +0530

    changes made to the project for PEP8 guidelines

--- media.py
+++ media.py
@@ -1,9 +1,10 @@
 import webbrowser
 
+
 class Movie():
     """ This class provide a way to store movie related information """
-    
-    def __init__(self, movie_title, movie_genres, movie_rating, movie_storyline, movie_poster, movie_trailer):
+    def __init__(self, movie_title, movie_genres, movie_rating,
+                 movie_storyline, movie_poster, movie_trailer):
         self.title = movie_title
         self.genre = movie_genres
         self.rating = movie_rating
@@ -11,8 +12,5 @@ class Movie():
         self.poster = movie_poster
         self.trailer = movie_trailer
 
-
     def show_trailer(self):
         webbrowser.open(self.trailer)
-    
-
